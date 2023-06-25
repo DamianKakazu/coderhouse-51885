7e08dec5bd270caa61c75010230fcdae32df96c8
Damian Kakazu <damian.kakazu@gmail.com>
2023-06-25 20:17:32 -0300

    Se desarrollo un fetch que carga los productos desde un archivo.json

--- scripts/EntregaFinal.js
+++ scripts/EntregaFinal.js
@@ -1,58 +1,12 @@
-//JSON: Productos Almacenados para Vender.
-const storedProducts = [
-    {
-        "idProducto" : "Product01",
-        "nombreProducto": "Cuadros", 
-        "descripcion": "¿Buscas el cuadro de bicicleta perfecto que sea ligero pero resistente? ¡No busque más! Estos son algunos consejos para elegir el mejor cuadro de bicicleta de carbono!",
-        "srcImages" : "./images/Productos/josh-nuttall-eTrHMJwI5ro-unsplash.jpg"
-    }, 
-    {
-        "idProducto" : "Product02",
-        "nombreProducto": "Vintage", 
-        "descripcion": "¿Busca un modo de transporte único y ecológico? ¡No busques más allá de la bicicleta de carbono vintage! Aquí hay algunos consejos para ayudarlo a comenzar.",
-        "srcImages" : "./images/Productos/murillo-de-paula-o2FCfhNSjPo-unsplash2.jpg"
-    },
-    {
-        "idProducto" : "Product03",
-        "nombreProducto": "El Futuro", 
-        "descripcion": "¡Hola, entusiastas de TuBici! ¡Tenemos un emocionante lanzamiento en camino! Prepárate para experimentar el futuro del ciclismo con nuestros nuevos cuadros y manubrios para bicicletas. Perfecto para todas tus aventuras en bicicleta y diseñado para brindarte la conducción más suave hasta el momento. ¡Estén atentos para más actualizaciones y prepárense para mejorar su juego de ciclismo!",
-        "srcImages" : "./images/Productos/harley-davidson-HG3Zy9sCCUg-unsplash2.jpg"
-    },    
-    {
-        "idProducto" : "Product04",
-        "nombreProducto": "Bicis", 
-        "descripcion": "¿Has estado pensando en comprar una bicicleta nueva pero no sabes por dónde empezar? ¡Las bicicletas de carbono pueden ser perfectas para ti! Aquí hay algunos consejos para ayudarlo a tomar la decisión correcta.",
-        "srcImages" : "./images/Productos/max-whitehead-4zKIbJjryOo-unsplash2.jpg"
-    },    
-    {
-        "idProducto" : "Product05",
-        "nombreProducto": "Manubrios", 
-        "descripcion": "¿Estás buscando formas de mejorar tu juego de ciclismo? ¡No busques más allá de los manubios de bicicleta de carbono! Aqui hay unas ideas para que puedas comenzar",
-        "srcImages" : "./images/Productos/josh-nuttall-zkVi57UYHIQ-unsplash.jpg "
-    }, 
-];
 
-const shoppingCart = {
 
-    objShoppingCartProducts : [],
+let shoppingCartProduct = [];
 
-    initProcess: ()=> {
-        //Hago un stringify sobre el array para procesarlo como JSON
-        const productosCarritosStr = JSON.stringify(storedProducts);
-        //Limpio por las dudas mi localStorage.
-        localStorage.clear();
-        //Seteo los productos del Carrito de compra.
-        localStorage.setItem("StoredProducts", productosCarritosStr);
-    },
+function createCardProducts(Products){
+    let products = document.querySelector("#listProducts");
 
-    getProductsFromStorage: () => {
-        
-        this.objShoppingCartProducts =  JSON.parse(localStorage.getItem("StoredProducts"));
-    },
 
-    setProductInShoppingCart:() => {
-        for(const producto of objShoppingCartProducts)
-        {
+        Products.forEach(producto => {
             const card = document.createElement("div");
             card.idProducto = producto.idProducto;
             card.className = "mainContainerProducto__tarjeta margen";
@@ -71,18 +25,75 @@ const shoppingCart = {
             card.innerHTML += "<br></br>";
             card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" Swal.fire('Quiere comprar ahora?','Esta seguro?','question') \"; >Comprrar ahora</button>";
 
-            let products = document.querySelector("#listProducts");
             products.appendChild(card);
+        });
 
-            console.log(card);
-        }
-    }
-} 
+}
 
 
-//Inicializando el proceso para emular la recuperación de productos del LocalStorage.
-shoppingCart.initProcess();
-//Recupera los productos del LocalStorage. 
-shoppingCart.getProductsFromStorage();
-//Muestro los productos en el Carrito de Compra.
-shoppingCart.setProductInShoppingCart();
+
+async function loadProducts()
+{
+    const response = await fetch('./data/Productos.json');
+    const jsonData = await response.json();
+    
+    createCardProducts(jsonData);
+    
+};
+
+
+
+
+
+this.loadProducts();
+// console.log(shoppingCartProduct);
+// if (shoppingCartProduct === Object && shoppingCartProduct.length > 0)
+// {
+//     let objShoppingCart = new ShoppingCart();
+//     objShoppingCart.shoppingCardProducts();
+// }
+// objShoppingCart.loadProducts();
+
+
+
+
+// class ShoppingCart
+// {
+//     shoppingCartProducts  = [];
+  
+    
+//     loadProducts()
+//     {
+//         async function loadJsonProduct(){
+//             const response = await fetch('./data/Productos.json');
+//             const jsonData = await response.json();
+//             let products = document.querySelector("#listProducts");
+//             jsonData.forEach(producto => {
+//                 const card = document.createElement("div");
+//                 card.idProducto = producto.idProducto;
+//                 card.className = "mainContainerProducto__tarjeta margen";
+    
+//                 const cardImage = document.createElement("img");
+//                 cardImage.src = producto.srcImages;
+    
+//                 card.appendChild(cardImage);
+    
+//                 card.innerHTML += "<h2>" + producto.nombreProducto + "</h2>";
+//                 card.innerHTML += "<p>" + producto.descripcion + "</p>";
+//                 card.innerHTML += "<br></br>";
+                
+//                 card.innerHTML += "<button id=" + producto.idProducto + " onclick=\"Swal.fire('Quiere agregar el producto al carrito?','Esta seguro?','question') \">Agregar al Carrito</button>";
+    
+//                 card.innerHTML += "<br></br>";
+//                 card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" Swal.fire('Quiere comprar ahora?','Esta seguro?','question') \"; >Comprrar ahora</button>";
+    
+//                 products.appendChild(card);
+//             })
+//         }
+//         loadJsonProduct();
+//     }
+// }
+
+// let objShoppingCart = new ShoppingCart();
+// objShoppingCart.loadProducts();
+
