b516d69ee10cbea47768f819eda0949451fc51a5
Damian Kakazu <damian.kakazu@gmail.com>
2023-08-05 00:23:29 -0300

    Cambios en el DOM. V3

--- scripts/EntregaFinal.js
+++ scripts/EntregaFinal.js
@@ -22,10 +22,10 @@ function createCardProducts(Products){
         
         //card.innerHTML += "<button id=" + producto.idProducto + " onclick=\"Swal.fire('Quiere agregar el producto al carrito?','Esta seguro?','question') \">Agregar al Carrito</button>";
         card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" agregarAlCarrito('" + producto.idProducto  +"'); \">Agregar al Carrito</button>";
-        card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" listarCarrito(); \">Listar Carrito</button>";
+        //card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" listarCarrito(); \">Listar Carrito</button>";
 
         card.innerHTML += "<br></br>";
-        card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" comprar() \"; >Comprrar ahora</button>";
+        //card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" comprar() \"; >Comprar ahora</button>";
 
         products.appendChild(card);
     });
@@ -84,6 +84,10 @@ function valideKey(evt){
 function borrarproductoseleccionado(productId){
     const elem = document.querySelector( '[id=prdSeleccionado_'+ productId +']');
     elem.remove();
+    //eliminar elemento del array
+    let elemento = shoppingCartProduct.find(item => item.idProducto == productId);
+    const index = selectedProduct.indexOf(elemento);
+    selectedProduct.splice(index, 1);
 }
 
 function agregarAlCarrito(productId){
@@ -171,35 +175,69 @@ function listarCarrito()
 function comprar()
 {
     let productos = "";
-    for(const producto of selectedProduct)
-    {
-        productos += producto.nombreProducto + "(Cantidad:" + producto.Cantidad + ")" + "\n";
-    }
 
-    Swal.fire({
-        title: 'Felicitaciones usted ha realizar correctamente la compra! Sus productos son: ' + productos,
-        showClass: {
-          popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass: {
-          popup: 'animate__animated animate__fadeOutUp'
+    if(selectedProduct.length > 0)
+    {
+        for(const producto of selectedProduct)
+        {
+            productos += producto.nombreProducto + "(Cantidad:" + producto.Cantidad + ")" + "\n";
         }
-       
-      })
-    
+
+        Swal.fire({
+            title: 'Felicitaciones usted ha realizar correctamente la compra! Sus productos son: ' + productos,
+            showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+            }
+        
+        })
+        
+        limpiar();
+    }
+    else
+    {
+        Swal.fire({ icon: 'warning', title: 'Warning', text: 'Su carrito está vacío.', footer: '' });
+    }
+}
+
+function borrar()
+{
+    if(selectedProduct.length > 0)
+    {
+        //Mensaje confirmación
+        Swal.fire({ title: 'Borrar', text: "Está seguro de borrar todo?", icon: 'warning', showCancelButton: true, confirmButtonColor: '#3085d6', cancelButtonColor: '#d33', confirmButtonText: 'Si, Borrar' }).then((result) => 
+        { if (result.isConfirmed) 
+            { limpiar();
+                Swal.fire( 'Borrado', 'El carrito se encuentra vacío.', 'success' ) } });
+    }
+    else
+    {
+        Swal.fire({ icon: 'warning', title: 'Warning', text: 'Su carrito está vacío.', footer: '' });
+    }
+}
+
+function limpiar()
+{    
     selectedProduct = [];
+
     //Limpio por las dudas mi localStorage.
     localStorage.clear();
     //Seteo los productos del Carrito de compra.
     localStorage.setItem("StoredSelectedProducts", "");
-}
 
+    let products = document.querySelector("#BuyProducts");
+    while (products.lastElementChild) {
+        products.removeChild(products.lastElementChild);
+    }
+}
 
 function ProductosSeleccionado(producto){
     let products = document.querySelector("#BuyProducts");
     
     const card = document.createElement("div");
-    card.idProducto = 'prdSeleccionado_' + producto.idProducto;
+    card.id = 'prdSeleccionado_' + producto.idProducto;
     card.className = "mainContainerProducto__tarjeta2 margen";
 
     card.innerHTML += "<h2> Producto Seleccionado para la Compra</h2>";
@@ -211,12 +249,8 @@ function ProductosSeleccionado(producto){
     card.innerHTML += "<button id=" + producto.idProducto + " onclick=\" borrarproductoseleccionado('" + producto.idProducto  +"'); \">Borrar producto</button>";
     card.innerHTML += "<br></br>";
     
-
     products.appendChild(card);
-
 }
-
-
 
 this.loadProductsShoppingCart();
 
